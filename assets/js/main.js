824ee11685a751766ebb9cb9ad99a14a208c2fd8
Anirban Mukherjee <anirban.loading@gmail.com>
2022-09-14 15:10:20 +0530

    add function to fetch nations by keyword

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,36 @@
 var inputBox = document.getElementById('tb-search')
 var displayBox = document.getElementById('tb-response')
 
-async function getCountry() {
-	let country = inputBox.value
-	let response = await fetch('https://restcountries.com/v3.1/name/'+country)
-	response = await response.text()
-	displayBox.value = response
+async function getNations() {
+	let keyword = inputBox.value
+	let response = null
+
+	if (keyword) {
+		response = await fetch('https://restcountries.com/v3.1/name/'+keyword)
+	}
+	else {
+		response = await fetch('https://restcountries.com/v3.1/all')
+	}
+	response = await response.json()
+
+	let no_of_nations = countNations(response)
+
+	displayBox.value = ""
+
+	if (no_of_nations > 1) {
+		for (i = 0; i < no_of_nations; i++) {
+			nation = response[i]
+			displayBox.value += nation["name"]["common"] + "\n"
+		}
+	}
+	else {
+		nation = response[0]
+		displayBox.value += nation["name"]["common"] + "\n"
+	}
+}
+
+function countNations(res) {
+	let len = res.length
+	console.log(len)
+	return len
 }
